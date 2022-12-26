858b7fd55087c8382cc5305c95e5f141d8673ed6
casco55 <juanjo.desarrollador@gmail.com>
2022-12-26 20:37:24 -0300

    fix repartos

--- src/helpers/endPoints.jsx
+++ src/helpers/endPoints.jsx
@@ -5,5 +5,5 @@ export const pathNameTekDelivery = "/repartos/despachos/asignar";
 export const pathNameSelfAssigned = "/repartos/asignados";
 export const pathNameEntrega = "/repartos/entrega/";
 export const pathNameCancelaEntrega = "/repartos/cancela/";
-//export const URL_local = "http://localhost:3000";
-export const URL_local = "https://mymolback-production.up.railway.app";
+export const URL_local = "http://localhost:3000";
+//export const URL_local = "https://mymolback-production.up.railway.app";
